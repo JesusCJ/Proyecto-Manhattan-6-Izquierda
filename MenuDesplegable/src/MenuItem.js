71ffa06f40b4b9a12a64e5a0585dd38a2301df34
JesusCJ <jesus_cuenca_jara@hotmail.com>
2017-01-10 17:05:14 +0100

    Modulo de menú desplegable
    Falta:
    -Cambiar imagen de iconos
    -Por determinar: secciones que contiene el menú (herramientas, login etc) y opciones (submenú) dentro de cada seccion (personalizar busqueda, acerca de nosotros...)

---
+++ MenuDesplegable/src/MenuItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class MenuItem extends Component {
+  render() {
+    return (
+      <div className="Category">
+      	<li>
+      	 <a href='#'>{this.props.name}</a>
+      	</li>
+      </div>
+    );
+  }
+}
+
+export default MenuItem;
