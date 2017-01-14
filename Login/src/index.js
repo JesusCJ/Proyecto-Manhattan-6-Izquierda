52185fd8158532fcf583d80098ce9da2e1d1c671
JesusCJ <jesus_cuenca_jara@hotmail.com>
2017-01-14 23:04:57 +0100

    Delete index.js

--- Login/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './Login';
-import './Login.css';
-
-ReactDOM.render(
-  <Login />,
-  document.getElementById('root')
-);
